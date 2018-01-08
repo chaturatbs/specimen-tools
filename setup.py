10a950fe7a97eefd3a6cb823ee36102ab626b9dd
Jose Cambronero <jcamsan@mit.edu>
2018-01-08 14:54:49 -0500

    fix bad setup

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     url='https://github.com/josepablocam/specimen-tools',
     classifiers=[ 
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers, Color Vision Scientists, Researchers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7'
